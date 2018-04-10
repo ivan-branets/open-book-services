2e9a5938d9dbb025d23f1f37aa3138599e64d2e0
Ivan Branets <Ivan_Branets@epam.com>
2018-04-10 15:39:25 +0300

    Updated to asset registry

--- api/controllers/contracts.js
+++ api/controllers/contracts.js
@@ -6,26 +6,53 @@ const Web3 = require('web3');
 const contract = require("truffle-contract");
 const fs = require("fs");
 
-const provider = new Web3.providers.HttpProvider("http://etherbokydum.westeurope.cloudapp.azure.com:8545");
+//const providerUrl = "http://etherbokydum.westeurope.cloudapp.azure.com:8545";
+const providerUrl = "http://localhost:8545";
+
+const provider = new Web3.providers.HttpProvider(providerUrl);
 const web3 = new Web3(provider);
+
 const AssetContractJson = JSON.parse(fs.readFileSync("build/contracts/AssetContract.json"));
+const AssetRegistryJson = JSON.parse(fs.readFileSync("build/contracts/AssetRegistry.json"));
 
-AssetContract = contract(AssetContractJson)
+const AssetContract = contract(AssetContractJson);
+const AssetRegistry = contract(AssetRegistryJson);
+
 AssetContract.setProvider(provider);
+AssetRegistry.setProvider(provider);
 
-AssetContract.defaults({
-    from: '0x4ebf52e2c78eb8233bc5ddcc32c5acb8f47c5266',
-    gas: 4712388
+let registry = null;
+
+web3.eth.getAccounts((error, accounts) => {
+    AssetContract.defaults({
+        from: accounts[0],
+        gas: 4712388
+    });
+
+    AssetRegistry.defaults({
+        from: accounts[0],
+        gas: 4712388
+    });
+});
+
+AssetRegistry.new().then(instance => {
+    registry = instance;
 });
 
 function putContact(req, res) {
     const contract = req.swagger.params.contract.value;
-    AssetContract.new(contract.assetId, web3.toWei(contract.assetPrise), contract.authorWalletAddresses)
-        .then(async (instance) => {
-            await instance.setRevenueRate(contract.authorWalletAddresses[0], 1000000);
+    registry.deployAssetContract(contract.assetId, web3.toWei(contract.assetPrise), contract.authorWalletAddresses)
+        .then(async txReceipt => {
+            const txLogs = txReceipt.logs[0];
+            const assetOwner = txLogs.args._owner;
+            const assetAddress = txLogs.args._asset;
+
+            const assetContract = await AssetContract.at(assetAddress);
+            await assetContract.setRevenueRate(contract.authorWalletAddresses[0], 1000000);
+
             res.status(201).json({
                 message: {
-                    contactAddress: instance.address
+                    contactAddress: assetContract.address
                 }
             });
         });
